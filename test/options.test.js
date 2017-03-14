2657c886a78189b097f782d26785bd6bff42ba66
Konstantin Gladkikh <birhoff@gmail.com>
2017-03-14 18:31:37 +0300

    test(options): Add options tests

---
+++ test/options.test.js
@@ -0,0 +1,66 @@
+const unified = require('unified');
+const markdown = require('remark-parse');
+
+const expect = require('chai').expect;
+
+const toBemjson = require('../index');
+
+const processor = unified().use(markdown);
+
+describe('Test converter options', () => {
+    describe('Test options.root', () => {
+
+        it('should `options.root=false` convert `root` to `documentation`', () => {
+            const tree = processor.parse('');
+            const bjson = toBemjson(tree);
+
+            expect(bjson).to.deep.equal({ block: 'documentation' });
+        });
+
+        it('should `options.root=myBlock` convert `root` to `myBlock`', () => {
+            const tree = processor.parse('');
+            const bjson = toBemjson(tree, { root: 'myBlock' });
+
+            expect(bjson).to.deep.equal({ block: 'myBlock' });
+        });
+
+    });
+
+    describe('Test options.scope', () => {
+
+        it('should `options.scope=false` convert `heading` to `block: heading`', () => {
+            const tree = processor.parse('# My heading');
+            const bjson = toBemjson(tree);
+
+            expect(bjson).to.deep.equal({
+                block: 'documentation',
+                content: [{
+                    block: 'heading',
+                    content: [{
+                        block: 'text',
+                        content: 'My heading'
+                    }]
+                }]
+            });
+        });
+
+        it('should `options.scope=true` convert `heading` to `elem: heading`', () => {
+            const tree = processor.parse('# My heading');
+            const bjson = toBemjson(tree, { scope: true, root: 'myBlock' });
+
+            expect(bjson).to.deep.equal({
+                block: 'myBlock',
+                content: [{
+                    block: 'myBlock',
+                    elem: 'heading',
+                    content: [{
+                        block: 'myBlock',
+                        elem: 'text',
+                        content: 'My heading'
+                    }]
+                }]
+            });
+        });
+
+    });
+});
